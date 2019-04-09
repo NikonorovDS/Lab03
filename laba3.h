753af69d095bfc909ba9c451f5ba8ed757838b2e
u211-05 <u211-05@public.mpei.local>
2019-04-09 16:40:14 +0300

    laba3 часть 3 вынос функции мин и макс

---
+++ laba3.h
@@ -0,0 +1,11 @@
+#ifndef LABA3_H_INCLUDED
+#define LABA3_H_INCLUDED
+
+#include <vector>
+using namespace std;
+//функция мин и макс
+void
+find_minmax(vector<double> numbers, double& min, double& max);
+
+
+#endif // LABA3_H_INCLUDED
