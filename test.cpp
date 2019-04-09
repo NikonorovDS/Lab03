e6f674dc746b42c0f35dae37b3d8c5d479928fb8
u211-05 <u211-05@public.mpei.local>
2019-04-09 17:09:02 +0300

    laba3 часть 3 новые тесты

--- test.cpp
+++ test.cpp
@@ -11,9 +11,53 @@ test_positive() {
     assert(max == 3);
 }
 
+
+void
+test_otric() {
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+
+void
+test_odinak() {
+    double min = 0;
+    double max = 0;
+    find_minmax({55, 55, 55}, min, max);
+    assert(min == 55);
+    assert(max == 55);
+}
+
+
+
+
+void
+test_odin() {
+    double min = 0;
+    double max = 0;
+    find_minmax({1337}, min, max);
+    assert(min == 1337);
+    assert(max == 1337);
+}
+
+
+void
+test_pusto() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == );
+    assert(max == );
+}
 int
 main()
 {
       test_positive();
-
+      test_otric();
+      test_odinak();
+      test_odin();
+      test_pusto();
 }
